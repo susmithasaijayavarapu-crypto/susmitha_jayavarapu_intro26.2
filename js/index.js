d34da6be7957f36f8a6cd1def65baffe0bc04899
susmithasaijayavarapu-crypto <susmithasaijayavarapu@gmail.com>
2026-05-19 11:52:55 -0500

    small changes added for structure

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ copyright.innerHTML = `&copy; Susmitha Jayavarapu ${thisYear}`;
 footer.appendChild(copyright);
 
 const skills =["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
-const skillsSection  = document.getElementById('Skills');
+const skillsSection  = document.getElementById('skills');
 const skillsList = skillsSection.querySelector('ul');
 for (let i=0; i < skills.length ; i++){
     const skill = document.createElement('li');
